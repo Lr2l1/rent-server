50cd70fb3acb1640f1737f3ab2dd4bd1135c770f
Lr2l1 <hoit5it@naver.com>
2024-05-09 16:40:46 +0900

    Craete car

--- src/main/java/rentServer/car/CarDao.java
+++ src/main/java/rentServer/car/CarDao.java
@@ -1,5 +1,76 @@
 package rentServer.car;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+
+import rentServer.user.UserDao;
+import rentServer.user.UserResponseDto;
+
 public class CarDao {
+
+	private Connection conn;
+	private PreparedStatement pstmt;
+	private ResultSet rs;
+
+	private CarDao() {
+		setConnection();
+	}
+
+	private static CarDao instance = new CarDao();
+
+	public static CarDao getInstance() {
+		return instance;
+	}
+
+	private void setConnection() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+
+			String url = "jdbc:mysql://localhost:3306/board_server_db";
+			String user = "root";
+			String password = "root";
+
+			this.conn = DriverManager.getConnection(url, user, password);
+
+			System.out.println("[DB 연동 성공]");
+		} catch (Exception e) {
+			System.err.println("[DB 연동 실패]");
+			e.printStackTrace();
+		}
+	}
+
+	public List<CarResponseDto> findUserAll() {
+		List<CarResponseDto> list = new ArrayList<CarResponseDto>();
+
+		try {
+
+			String sql = "SELECT code, name, brand, type, fuel, seater, price FROM cars";
+			pstmt = conn.prepareStatement(sql);
+
+			rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+				String code = rs.getString(1);
+				String name = rs.getString(2);
+				String brand = rs.getString(3);
+				String type = rs.getString(4);
+				String fuel = rs.getString(5);
+				String seater = rs.getString(6);
+				String price = rs.getString(7);
+
+				CarResponseDto user = new CarResponseDto(code, name, brand, type, fuel, seater, price);
+				list.add(user);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return list;
+	}
 
 }
--- src/main/java/rentServer/car/CarResponseDto.java
+++ src/main/java/rentServer/car/CarResponseDto.java
@@ -1,5 +1,116 @@
 package rentServer.car;
 
-public class CarResponseDto {
+import java.security.Timestamp;
 
+public class CarResponseDto {
+	private String code;
+	private String name;
+	private String brand;
+	private String type;
+	private String fuel;
+	private String seater;
+	private String price;
+	private Timestamp regDate;
+	private Timestamp modDate;
+	
+	
+	public CarResponseDto(String code, String name, String brand, String type, String fuel, String seater, String price,
+			Timestamp regDate, Timestamp modDate) {
+		super();
+		this.code = code;
+		this.name = name;
+		this.brand = brand;
+		this.type = type;
+		this.fuel = fuel;
+		this.seater = seater;
+		this.price = price;
+		this.regDate = regDate;
+		this.modDate = modDate;
+	}
+	
+	public CarResponseDto(String code, String name, String brand, String type, String fuel, String seater, String price) {
+		super();
+		this.code = code;
+		this.name = name;
+		this.brand = brand;
+		this.type = type;
+		this.fuel = fuel;
+		this.seater = seater;
+		this.price = price;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getFuel() {
+		return fuel;
+	}
+
+	public void setFuel(String fuel) {
+		this.fuel = fuel;
+	}
+
+	public String getSeater() {
+		return seater;
+	}
+
+	public void setSeater(String seater) {
+		this.seater = seater;
+	}
+
+	public String getPrice() {
+		return price;
+	}
+
+	public void setPrice(String price) {
+		this.price = price;
+	}
+
+	public Timestamp getRegDate() {
+		return regDate;
+	}
+
+	public void setRegDate(Timestamp regDate) {
+		this.regDate = regDate;
+	}
+
+	public Timestamp getModDate() {
+		return modDate;
+	}
+
+	public void setModDate(Timestamp modDate) {
+		this.modDate = modDate;
+	}
+	
+	
+	
 }
